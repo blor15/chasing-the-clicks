8ea20187e58d4dd23c785ed1656b5fe8cc5fc6c9
Brandon <blor15@hotmail.com>
2023-04-14 17:25:49 -0600

    clear functionality added

--- src/App.js
+++ src/App.js
@@ -14,12 +14,17 @@ function App() {
     setCounter((count) => count + 1);
   };
 
+  const clear = () => {
+    setCounter((count) => count === localStorage.clear());
+  };
+
   return (
     <div className="counter">
       <h1>Chasing The Clicks</h1>
       <h2>{counter}</h2>
       <div className="click-me">
         <button onClick={increase}>Click Me</button>
+        <button onClick={clear}>Clear</button>
       </div>
     </div>
   );
